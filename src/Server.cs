cbc553cae5e3da0617ffd95cf19f7719c41b0f1c
alicanipek <alican.ipek@outlook.com>
2025-10-03 23:34:16 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,5 +7,30 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-using Socket socket = server.AcceptSocket(); // wait for client
-socket.Send(System.Text.Encoding.ASCII.GetBytes("+PONG\r\n"));
+
+// Buffer for reading data
+Byte[] bytes = new Byte[256];
+
+// Enter the listening loop.
+while (true)
+{
+    Console.Write("Waiting for a connection... ");
+
+    // Perform a blocking call to accept requests.
+    // You could also use server.AcceptSocket() here.
+    using TcpClient client = server.AcceptTcpClient();
+
+    // Get a stream object for reading and writing
+    NetworkStream stream = client.GetStream();
+
+    int i;
+
+    // Loop to receive all the data sent by the client.
+    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+    {
+        byte[] msg = System.Text.Encoding.ASCII.GetBytes("+PONG\r\n");
+
+        // Send back a response.
+        stream.Write(msg, 0, msg.Length);
+    }
+}
