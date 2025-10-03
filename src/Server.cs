17afbb07fa0dd03eb7c4a420d7c83ab02dacf892
alicanipek <alican.ipek@outlook.com>
2025-10-03 23:30:11 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,5 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+using Socket socket = server.AcceptSocket(); // wait for client
+socket.Send(System.Text.Encoding.ASCII.GetBytes("+PONG\r\n"));
