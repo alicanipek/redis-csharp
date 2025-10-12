7fbd8f2cf688a992f9dc4e9ff3edc41e300a0390
alicanipek <alican.ipek@outlook.com>
2025-10-12 23:14:10 +0300

    codecrafters submit [skip ci]

--- src/CommandHandlers/WaitCommandHandler.cs
+++ src/CommandHandlers/WaitCommandHandler.cs
@@ -1,15 +1,16 @@
 using System;
 using codecrafters_redis.src.Infrastructure;
+using codecrafters_redis.src.Replica;
 
 namespace codecrafters_redis.src.CommandHandlers;
 
-public class WaitCommandHandler : ICommandHandler
+public class WaitCommandHandler(ReplicaManager replicaManager) : ICommandHandler
 {
     public string CommandName => "WAIT";
     public bool IsWriteCommand => true; 
 
     public Task<byte[]> HandleAsync(List<object> arguments)
     {
-        return Task.FromResult(RespParser.EncodeInteger(0));
+        return Task.FromResult(RespParser.EncodeInteger(replicaManager.ReplicaCount));
     }
 }
