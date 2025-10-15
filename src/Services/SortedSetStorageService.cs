7d2fa7ff77f56cf971764ecab3b60e8b8a164c5c
alicanipek <alican.ipek@outlook.com>
2025-10-15 21:37:33 +0300

    codecrafters submit [skip ci]

--- src/Services/SortedSetStorageService.cs
+++ src/Services/SortedSetStorageService.cs
@@ -3,7 +3,20 @@ using System.Collections.Concurrent;
 
 namespace codecrafters_redis.src.Services;
 
-public class SetItem : IComparable<SetItem>
+// public class SetItemComparer : IComparer<SetItem>
+// {
+//     public int Compare(SetItem? x, SetItem? y)
+//     {
+//         if (x == null && y == null) return 0;
+//         if (x == null) return -1;
+//         if (y == null) return 1;
+
+//         var scoreComparison = x.Score.CompareTo(y.Score);
+//         return scoreComparison != 0 ? scoreComparison : -1;
+//     }
+// }
+
+public class SetItem : IComparable<SetItem>, IEquatable<SetItem>
 {
     public double Score { get; set; }
     public string Member { get; set; }
@@ -21,25 +34,33 @@ public class SetItem : IComparable<SetItem>
         var scoreComparison = Score.CompareTo(other.Score);
         return scoreComparison != 0 ? scoreComparison : string.Compare(Member, other.Member, StringComparison.Ordinal);
     }
+
+    public bool Equals(SetItem? other)
+    {
+        if (other == null) return false;
+        return Member == other.Member;
+    }
 }
 
 public class SortedSetStorageService
 {
-    private readonly ConcurrentDictionary<string, SortedSet<SetItem>> SortedSets = new();
+    private readonly ConcurrentDictionary<string, SortedDictionary<string, double>> SortedSets = new();
 
     public int ZAdd(string key, IEnumerable<SetItem> members)
     {
         var sortedSet = SortedSets.GetOrAdd(key, _ => []);
-
         int addedCount = 0;
         foreach (var member in members)
         {
-            if (sortedSet.Add(member))
+            if (sortedSet.TryAdd(member.Member, member.Score))
             {
                 addedCount++;
             }
+            else
+            {
+                sortedSet[member.Member] = member.Score; // Update score if member exists
+            }
         }
-
         return addedCount;
     }
 }
